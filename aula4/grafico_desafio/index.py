700556f50089bad90e34926af0dec8400eb9be3d
EngMarianaBrito <marianabrito1791@gmail.com>
2021-09-01 15:19:53 -0300

    Modoliraçao com graficos

--- aula4/grafico_desafio/index.py
+++ aula4/grafico_desafio/index.py
@@ -4,15 +4,16 @@ import numpy as np
 
 def grafico_1():
 
-    dados_tabela_aluno_um = [10,5,2,4,6,8]
-    dados_tabela_aluno_dois = [ 1,2,4,8,7,4]
+    dados_tabela_aluno_um = [1,3,5,7,9]
+    dados_tabela_aluno_dois = [2,4,6,8,10]
+   
     x = 10*np.array(range(len(dados_tabela_aluno_um)))
 
     plt.plot( x, dados_tabela_aluno_um, 'go') # green bolinha
-    plt.plot( x, dados_tabela_aluno_um, 'k:', color='orange') # linha pontilha orange
+    plt.plot( x, dados_tabela_aluno_um, 'k:', color='blue') # linha pontilha orange
 
     plt.plot( x, dados_tabela_aluno_dois, 'r^') # red triangulo
-    plt.plot( x, dados_tabela_aluno_dois, 'k--', color='blue')  # linha tracejada azul
+    plt.plot( x, dados_tabela_aluno_dois, 'k--', color='red')  # linha tracejada azul
 
     plt.axis([-10, 60, 0, 11])
     plt.title("Grafico - Lanche")
@@ -21,3 +22,7 @@ def grafico_1():
     plt.xlabel("Dias")
     plt.ylabel("Consumo")
     plt.show()
+
+def grafico_2():
+    produtos = []
+    produtos.append
