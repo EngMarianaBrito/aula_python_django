d9eaee1bf3d34fcd00d2f1d9d851e707b70d0ebd
EngMarianaBrito <marianabrito1791@gmail.com>
2021-09-01 15:18:58 -0300

    /bin/python3 /home/lia/Documentos/Python/python_aula/aula4/grafico_desafio/index.py

---
+++ aula4/grafico_desafio/index.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def grafico_1():
+
+    dados_tabela_aluno_um = [10,5,2,4,6,8]
+    dados_tabela_aluno_dois = [ 1,2,4,8,7,4]
+    x = 10*np.array(range(len(dados_tabela_aluno_um)))
+
+    plt.plot( x, dados_tabela_aluno_um, 'go') # green bolinha
+    plt.plot( x, dados_tabela_aluno_um, 'k:', color='orange') # linha pontilha orange
+
+    plt.plot( x, dados_tabela_aluno_dois, 'r^') # red triangulo
+    plt.plot( x, dados_tabela_aluno_dois, 'k--', color='blue')  # linha tracejada azul
+
+    plt.axis([-10, 60, 0, 11])
+    plt.title("Grafico - Lanche")
+
+    plt.grid(True)
+    plt.xlabel("Dias")
+    plt.ylabel("Consumo")
+    plt.show()
---
+++ aula4/grafico_desafio/main.py
@@ -0,0 +1,3 @@
+from index import grafico_1 as grafico_linha
+
+grafico_linha()
