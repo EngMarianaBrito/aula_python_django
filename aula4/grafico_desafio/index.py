7023d456b2e4d48b6e5ad1acf4aeabf0f8042e49
EngMarianaBrito <marianabrito1791@gmail.com>
2021-09-02 14:18:16 -0300

    Grafico Desafio

--- aula4/grafico_desafio/index.py
+++ aula4/grafico_desafio/index.py
@@ -1,28 +1,49 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
 import numpy as np
 
+def grafico_1():  
+    dados= []
+    while len(dados) < 5:
+        dados_d = input("Escreva um numero: ")
+        if dados_d in dados:
+            print("Nao pode escrever esse numero.")
+        else:
+            dados.append(dados_d)
 
-def grafico_1():
-
-    dados_tabela_aluno_um = [1,3,5,7,9]
-    dados_tabela_aluno_dois = [2,4,6,8,10]
-   
-    x = 10*np.array(range(len(dados_tabela_aluno_um)))
-
-    plt.plot( x, dados_tabela_aluno_um, 'go') # green bolinha
-    plt.plot( x, dados_tabela_aluno_um, 'k:', color='blue') # linha pontilha orange
-
-    plt.plot( x, dados_tabela_aluno_dois, 'r^') # red triangulo
-    plt.plot( x, dados_tabela_aluno_dois, 'k--', color='red')  # linha tracejada azul
-
-    plt.axis([-10, 60, 0, 11])
-    plt.title("Grafico - Lanche")
-
-    plt.grid(True)
-    plt.xlabel("Dias")
-    plt.ylabel("Consumo")
+    plt.pie(dados)
+    plt.savefig('grafico_gantt.png', transparent = True)#salvar grafico
     plt.show()
 
 def grafico_2():
-    produtos = []
-    produtos.append
+    fig, gantt = plt.subplots() 
+    gantt.set_ylim(0, 50) 
+
+    gantt.set_yticks([15, 25, 35]) 
+    gantt.set_yticklabels(['1', '2', '3']) 
+    
+    gantt.grid(True) 
+    
+    gantt.broken_barh([(40, 50)], (30, 9), facecolors =('tab:red')) 
+    gantt.broken_barh([(110, 10), (150, 10)], (10, 9), 
+                            facecolors ='tab:blue') 
+    
+    gantt.broken_barh([(10, 50), (100, 20), (130, 10)], (20, 9), 
+                                    facecolors =('tab:green')) 
+    
+    
+    plt.style.use('ggplot')#mudar estilo
+    plt.savefig('grafico_gantt.png', transparent = True)#salvar grafico
+    plt . show ()
+
+def grafico_3():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    dado_1, dado_2 ,dado_3 = axes3d.get_test_data(0.10)#implementando dados
+    
+    ax.plot_wireframe( dado_1, dado_2 ,dado_3,rstride=20, cstride=20)
+    
+    plt.style.use('ggplot')#mudar estilo
+    plt.savefig('grafico_gantt.png', transparent = True)#salvar grafico
+    plt.show()
--- aula4/grafico_desafio/main.py
+++ aula4/grafico_desafio/main.py
@@ -1,3 +1,9 @@
-from index import grafico_1 as grafico_linha
+from index import grafico_1 as grafico_pizza
+from index import grafico_2 as graf_2
+from index import grafico_3 as graf_3
 
-grafico_linha()
+grafico_pizza()
+
+graf_2()
+
+graf_3()
